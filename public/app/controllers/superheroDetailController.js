d95f80e30c5688f2a8c3dba7544551b1ef6aa541
Lance Fallon <lancefallon@Lances-MacBook-Pro.local>
2015-10-03 19:18:09 -0400

    add captability to pass server side values to anglar

--- public/app/controllers/superheroDetailController.js
+++ public/app/controllers/superheroDetailController.js
@@ -3,6 +3,8 @@
         '$rootScope', '$scope', '$http', '$state', '$stateParams', 'superheroService',
         function($rootScope, $scope, $http, $state, $stateParams, superheroService){
 
+            $scope.myParam;
+
             $scope.superhero = {};
 
             $scope.init = function(){
@@ -12,6 +14,11 @@
                     console.log(err.status);
                 })
             };
+
+            //used to initialize values passed from the server
+            $scope.initServerValues = function(param){
+                $scope.myParam = param === undefined ? 'myParam' : param;
+            }
 
             $scope.linkHome = function(){
                 $state.go('home');
