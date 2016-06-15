e7636560643dff23427c71fc9ffcf02eee6e71a9
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-06-15 14:50:04 -0400

    key

--- server/services/youtubeAPI.js
+++ server/services/youtubeAPI.js
@@ -3,7 +3,7 @@ var http = require('request-promise');
 
 //var apiKey = process.env.YOUTUBE_API_KEY || 'AIzaSyB3v4vF0MIHB00iTr4lAxW2ONwZNmTR0HM';
 
-var apiKey = 'AIzaSyAdvomXbhYg3GeBGymbPVBg-aRJeIOfFyQ';
+var apiKey = 'AIzaSyAY8aVa_oVZya_-a53oyFikvs-RwJfNDuk';
 
 var _options = {};
 
