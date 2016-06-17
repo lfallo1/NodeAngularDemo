213caa3d77fd25506f141725ab47939e7b3dbf72
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-06-17 11:06:10 -0400

    config

--- public/app/app.js
+++ public/app/app.js
@@ -25,12 +25,12 @@ angular.module('youtubeSearchApp', ['ui.router','ngRoute', 'ngAnimate', 'toaster
     }]).run(['$rootScope', '$log', '$document', 'AuthService', function($rootScope, $log, $document, AuthService){
 
         //live
-        //$rootScope.clientId = "613015363976-vt1eeel6upnq26k2haupepbdtpd2bjgj.apps.googleusercontent.com";
-        //$rootScope.authCallbackUrl = "http://www.youtubeagent.io/oauthcallback";
+        $rootScope.clientId = "613015363976-vt1eeel6upnq26k2haupepbdtpd2bjgj.apps.googleusercontent.com";
+        $rootScope.authCallbackUrl = "http://www.youtubeagent.io/oauthcallback";
 
         //local
-        $rootScope.clientId = "613015363976-0aodg2ib3dmv8m2g7gmknnglg29cmir9.apps.googleusercontent.com";
-        $rootScope.authCallbackUrl = "http://localhost:3000/oauthcallback";
+        //$rootScope.clientId = "613015363976-0aodg2ib3dmv8m2g7gmknnglg29cmir9.apps.googleusercontent.com";
+        //$rootScope.authCallbackUrl = "http://localhost:3000/oauthcallback";
 
         //set AuthService on rootScope for convenience (still placing AuthService in its service for modularity)
         $rootScope.AuthService = AuthService;
