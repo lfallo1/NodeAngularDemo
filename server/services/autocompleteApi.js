66a455f42402a5b678a1345f6c25c8271bd4757b
Lance Fallon <lfallon@sbgtv.com>
2017-04-25 13:47:10 -0400

    change autocomplete endpoint to use live google endpoint

--- server/services/autocompleteApi.js
+++ server/services/autocompleteApi.js
@@ -3,7 +3,9 @@ var http = require('request-promise');
 var express = require('express');
 var router = express.Router();
 
-var AUTOCOMPLETE_BASE = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+// var AUTOCOMPLETE_BASE = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+var AUTOCOMPLETE_BASE = 'https://www.google.com/complete/search?client=hp&hl=en&xhr=t&q=';
+var REGEX_STRIP_TAGS = /(<([^>]+)>)|(&lt;([^>]+)&gt;)/ig;
 
 var _options = {};
 
@@ -19,8 +21,10 @@ router.get('', function (req, res, next) {
     _options = options;
     http(options).then(function (data) {
         var response = {results: []};
-        if(data && data.length > 0){
-          res.json(data[1]);
+        if(data && data.length > 1){
+          // res.json(data[1].map(function(d){return d[0];}));
+          var responseArray = data[1].map(function(d){return d[0] ? d[0].replace(REGEX_STRIP_TAGS,'') : '';})
+          res.json(responseArray.filter(function(d){return d}));
         } else{
           res.json([]);
         }
